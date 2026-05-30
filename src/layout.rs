3944c4637177d1e095a07ba229c069e99fcfe5b6
wellscrosby <wellscrosby@gmail.com>
2026-05-29 19:42:10 -0700

    remove unused component and stderr

--- src/layout.rs
+++ src/layout.rs
@@ -109,10 +109,6 @@ pub struct HierarchyFilter;
 #[derive(Component)]
 pub struct Toolbar;
 
-/// Root UI node for the editor shell (flush with the OS window client area).
-#[derive(Component)]
-pub struct EditorLayoutRoot;
-
 pub fn editor_layout(
     icon_font: &IconFont,
     editor_font: &jackdaw_feathers::icons::EditorFont,
@@ -120,7 +116,6 @@ pub fn editor_layout(
 ) -> impl Bundle {
     (
         EditorEntity,
-        EditorLayoutRoot,
         crate::window_chrome::WindowShellRoot,
         BackgroundColor(tokens::WINDOW_BG),
         Node {
